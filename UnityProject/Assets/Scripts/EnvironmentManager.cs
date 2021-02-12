d76f01f6b7ee181f6c30a606974751b9a36c0365
daversd <dave_rsd@hotmail.com>
2021-02-12 00:34:18 -0300

    First Commit

---
+++ UnityProject/Assets/Scripts/EnvironmentManager.cs
@@ -0,0 +1,69 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System.Diagnostics;
+using System.Linq;
+
+public class EnvironmentManager : MonoBehaviour
+{
+    #region Fields and properties
+
+    VoxelGrid _voxelGrid;
+    int _randomSeed = 666;
+
+    bool _showVoids = true;
+
+    #endregion
+
+    #region Unity Standard Methods
+
+    void Start()
+    {
+        // Initialise the voxel grid
+        Vector3Int gridSize = new Vector3Int(64, 10, 64);
+        _voxelGrid = new VoxelGrid(gridSize, Vector3.zero, 1, parent: this.transform);
+
+        // Set the random engine's seed
+        Random.InitState(_randomSeed);
+    }
+
+    void Update()
+    {
+        // Draw the voxels according to their Function Colors
+        DrawVoxels();
+
+        // Use the V key to switch between showing voids
+        if (Input.GetKeyDown(KeyCode.V))
+        {
+            _showVoids = !_showVoids;
+        }
+    }
+
+    #endregion
+
+    #region Private Methods
+
+    /// <summary>
+    /// Draws the voxels according to it's state and Function Corlor
+    /// </summary>
+    void DrawVoxels()
+    {
+        foreach (var voxel in _voxelGrid.Voxels)
+        {
+            if (voxel.IsActive)
+            {
+                Vector3 pos = (Vector3)voxel.Index * _voxelGrid.VoxelSize + transform.position;
+                if (voxel.FColor    ==   FunctionColor.Black)   Drawing.DrawCube(pos, _voxelGrid.VoxelSize, Color.black);
+                else if (voxel.FColor == FunctionColor.Red)     Drawing.DrawCube(pos, _voxelGrid.VoxelSize, Color.red);
+                else if (voxel.FColor == FunctionColor.Yellow)  Drawing.DrawCube(pos, _voxelGrid.VoxelSize, Color.yellow);
+                else if (voxel.FColor == FunctionColor.Green)   Drawing.DrawCube(pos, _voxelGrid.VoxelSize, Color.green);
+                else if (voxel.FColor == FunctionColor.Cyan)    Drawing.DrawCube(pos, _voxelGrid.VoxelSize, Color.cyan);
+                else if (voxel.FColor == FunctionColor.Magenta) Drawing.DrawCube(pos, _voxelGrid.VoxelSize, Color.magenta);
+                else if (_showVoids && voxel.Index.y == 0)
+                    Drawing.DrawTransparentCube(pos, _voxelGrid.VoxelSize);
+            }
+        }
+    }
+
+    #endregion
+}
---
+++ UnityProject/Assets/Scripts/Pix2Pix.cs
@@ -0,0 +1,85 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Unity.Barracuda;
+
+public class Pix2Pix
+{
+    #region Fields and Properties
+
+    NNModel _modelAsset;
+    Model _loadedModel;
+    IWorker _worker;
+
+    #endregion
+
+    #region Constructor
+
+    /// <summary>
+    /// Constructor for a regualar Pix2Pix inference object
+    /// </summary>
+    public Pix2Pix()
+    {
+        // Initialise the model
+        _modelAsset = Resources.Load<NNModel>("Models/Pix2Pix_Blobs");
+        _loadedModel = ModelLoader.Load(_modelAsset);
+        _worker = WorkerFactory.CreateWorker(WorkerFactory.Type.ComputePrecompiled, _loadedModel);
+    }
+
+    #endregion
+
+    #region Public Methods
+    
+
+    #endregion
+
+    #region Private Methods
+
+    /// <summary>
+    /// Translates a Tensor into a Texture2D
+    /// </summary>
+    /// <param name="inputTensor">The Tensor to be translated</param>
+    /// <param name="inputTexture">A reference Texture2D for formatting</param>
+    /// <returns></returns>
+    Texture2D Tensor2Image(Tensor inputTensor, Texture2D inputTexture)
+    {
+        //Apply output tensor to a temp RenderTexture
+        var tempRT = new RenderTexture(256, 256, 24);
+        inputTensor.ToRenderTexture(tempRT);
+        RenderTexture.active = tempRT;
+
+        //Assign temp RenderTexture to a new Texture2D
+        var resultTexture = new Texture2D(inputTexture.width, inputTexture.height);
+        resultTexture.ReadPixels(new Rect(0, 0, tempRT.width, tempRT.height), 0, 0);
+        resultTexture.Apply();
+
+        //Destroy temp RenderTexture
+        RenderTexture.active = null;
+        tempRT.DiscardContents();
+
+        return resultTexture;
+    }
+
+    /// <summary>
+    /// Normalizes a tensor to a target range
+    /// </summary>
+    /// <param name="inputTensor"></param>
+    /// <param name="a1">Original range minimum</param>
+    /// <param name="a2">Original range maximum</param>
+    /// <param name="b1">Target range minimum</param>
+    /// <param name="b2">Target range maximum</param>
+    /// <returns>The normalised tensor</returns>
+    Tensor NormaliseTensor(Tensor inputTensor, float a1, float a2, float b1, float b2)
+    {
+        var data = inputTensor.data.Download(inputTensor.shape);
+        float[] normalized = new float[data.Length];
+        for (int i = 0; i < data.Length; i++)
+        {
+            normalized[i] = Util.Normalise(data[i], a1, a2, b1, b2);
+        }
+
+        return new Tensor(inputTensor.shape, normalized);
+    }
+
+    #endregion
+}
